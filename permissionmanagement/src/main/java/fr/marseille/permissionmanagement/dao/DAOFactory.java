8b1964d2e5408c0e5523085fc253d99a8130c1fa
Antony <Antony@192.168.1.17>
2016-07-19 21:04:46 +0200

    Commit depuis la maison pour tester les modifs à partir d'un clone du remote.
    Ajout de l'attribut UserDao et d'un accesseur statique singleton-like

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/DAOFactory.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/DAOFactory.java
@@ -1,10 +1,22 @@
 package fr.marseille.permissionmanagement.dao;
 
 import fr.marseille.permissionmanagement.dao.impl.RightJPADAO;
+import fr.marseille.permissionmanagement.dao.impl.UserJPADAO;
 
 public class DAOFactory {
 
     private static RightDAO right;
+    
+    // added from home
+    private static UserDAO user;
+    // added from home
+    public static UserDAO getUserDAO() {
+    	if(null == user) {
+    		user = new UserJPADAO();
+    	}
+    	
+    	return user;
+    }
 
     public static RightDAO getRightDAO() {
 
