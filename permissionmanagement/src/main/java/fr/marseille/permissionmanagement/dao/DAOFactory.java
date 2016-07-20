fbe96abd656b7d738e77c73476babadf9f5d0fd9
asus-hb <asus-hb@DESKTOP-741VTDM>
2016-07-20 09:00:53 +0200

    Rajout de l'attribut ProfilDAO et de son accesseur pseudo-singleton

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/DAOFactory.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/DAOFactory.java
@@ -1,29 +1,41 @@
 package fr.marseille.permissionmanagement.dao;
 
+import fr.marseille.permissionmanagement.dao.impl.ProfileJPADAO;
 import fr.marseille.permissionmanagement.dao.impl.RightJPADAO;
 import fr.marseille.permissionmanagement.dao.impl.UserJPADAO;
 
 public class DAOFactory {
 
-    private static RightDAO right;
-    
-    // added from home
-    private static UserDAO user;
-    // added from home
-    public static UserDAO getUserDAO() {
-    	if(null == user) {
-    		user = new UserJPADAO();
-    	}
-    	
-    	return user;
-    }
+	private static RightDAO right;
 
-    public static RightDAO getRightDAO() {
+	private static ProfileDAO profile;
 
-        if (null == right) {
-            right = new RightJPADAO();
-        }
+	// added from home
+	private static UserDAO user;
 
-        return right;
-    }
+	// added from home
+	public static UserDAO getUserDAO() {
+		if (null == user) {
+			user = new UserJPADAO();
+		}
+
+		return user;
+	}
+
+	public static ProfileDAO getProfileDAO() {
+		if (null == profile) {
+			profile = new ProfileJPADAO();
+		}
+
+		return profile;
+	}
+
+	public static RightDAO getRightDAO() {
+
+		if (null == right) {
+			right = new RightJPADAO();
+		}
+
+		return right;
+	}
 }
