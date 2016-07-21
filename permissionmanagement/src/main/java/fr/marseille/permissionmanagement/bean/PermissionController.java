a9b336f299bb13d95378104abbc43f60af6896c5
antony <zink13013@gmail.com>
2016-07-21 09:31:10 +0200

    Ajout des fichiers index.xhtml pour chaque bean

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/bean/PermissionController.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/bean/PermissionController.java
@@ -3,47 +3,46 @@ package fr.marseille.permissionmanagement.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name = "HelloController")
+@ManagedBean(name = "PermissionController")
 @RequestScoped
 public class PermissionController {
 
-	private String message = "";
-	private String password = "1234";
+    private String message  = "";
+    private String password = "1234";
 
-	public PermissionController() {
-		super();
-	}
+    public PermissionController() {
+        super();
+    }
 
-	/**
-	 * ajout d'une methode publique me permettant de faire une redirection
-	 * dynamique vers une autre page
-	 */
+    /**
+     * ajout d'une methode publique me permettant de faire une redirection dynamique vers une autre page
+     */
 
-	public String nextPage() {
-		if (message.isEmpty()) {
-			return null;
-		}
-		return "redirectPage";
-	}
+    public String nextPage() {
+        if (message.isEmpty()) {
+            return null;
+        }
+        return "redirectPage";
+    }
 
-	public String nextPage(String page) {
-		return page;
-	}
+    public String nextPage(String page) {
+        return page;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
