0bf84ac9697157454e8768454747eabda06e5067
asus-hb <asus-hb@DESKTOP-741VTDM>
2016-07-21 10:27:47 +0200

    creation package View modification controller et index de user

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/bean/UserController.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/bean/UserController.java
@@ -3,47 +3,26 @@ package fr.marseille.permissionmanagement.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean(name = "HelloController")
+@ManagedBean(name = "UserController")
 @RequestScoped
 public class UserController {
 
-	private String message = "";
-	private String password = "1234";
+    private String message = "Welcome to the Users'page ";
 
-	public UserController() {
-		super();
-	}
+    public UserController() {
+        super();
+    }
 
-	/**
-	 * ajout d'une methode publique me permettant de faire une redirection
-	 * dynamique vers une autre page
-	 */
+    public String nextPage(String page) {
+        return page;
+    }
 
-	public String nextPage() {
-		if (message.isEmpty()) {
-			return null;
-		}
-		return "redirectPage";
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String nextPage(String page) {
-		return page;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 }
