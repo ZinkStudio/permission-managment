bdb04eb940a321d592035f3e606843b75dae2491
asus-hb <asus-hb@DESKTOP-741VTDM>
2016-07-21 09:19:26 +0200

    Ajout des ManagedBean dans un package bean

---
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/bean/ProfileController.java
@@ -0,0 +1,49 @@
+package fr.marseille.permissionmanagement.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "HelloController")
+@RequestScoped
+public class ProfileController {
+
+	private String message = "";
+	private String password = "1234";
+
+	public ProfileController() {
+		super();
+	}
+
+	/**
+	 * ajout d'une methode publique me permettant de faire une redirection
+	 * dynamique vers une autre page
+	 */
+
+	public String nextPage() {
+		if (message.isEmpty()) {
+			return null;
+		}
+		return "redirectPage";
+	}
+
+	public String nextPage(String page) {
+		return page;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
