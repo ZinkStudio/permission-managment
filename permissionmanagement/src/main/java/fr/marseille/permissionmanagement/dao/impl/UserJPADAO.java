8d605fb53a77993a5b73490c235591b2cb637060
asus-hb <asus-hb@DESKTOP-741VTDM>
2016-07-24 21:26:03 +0200

    Mise a jour de l'implementation des methodes de la DAO des users avec la classe JPAUtil (testant l'unicite de la transaction)

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/impl/UserJPADAO.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/dao/impl/UserJPADAO.java
@@ -2,9 +2,6 @@ package fr.marseille.permissionmanagement.dao.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import org.apache.log4j.Logger;
 import fr.marseille.permissionmanagement.dao.UserDAO;
 import fr.marseille.permissionmanagement.exception.DAOException;
@@ -25,17 +22,13 @@ public class UserJPADAO implements UserDAO {
 
     @Override
     public boolean save(User user) throws DAOException {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("permissionmanagement");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
         try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(user);
-            entityManager.getTransaction().commit();
-
+            JPAUtil.beginTransaction();
+            JPAUtil.getEntityManager().persist(user);
+            JPAUtil.commitTransaction();
         } catch (RuntimeException e) {
-            entityManager.close();
-            entityManagerFactory.close();
-            String msg = "persist : " + e.getMessage();
+            JPAUtil.closeAll();
+            String msg = "save : " + e.getMessage();
             LOG.warn(msg);
             throw new DAOException(msg, e);
         }
@@ -44,14 +37,11 @@ public class UserJPADAO implements UserDAO {
 
     @Override
     public List<User> findAll() throws DAOException {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("permissionmanagement");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
         List<User> users = new ArrayList<>();
         try {
-            users = (List<User>) entityManager.createQuery("from User").getResultList();
+            users = (List<User>) JPAUtil.getEntityManager().createQuery("from User").getResultList();
         } catch (RuntimeException e) {
-            entityManager.close();
-            entityManagerFactory.close();
+            JPAUtil.closeAll();
             String msg = "findAll : " + e.getMessage();
             LOG.warn(msg);
             throw new DAOException(msg, e);
@@ -61,16 +51,10 @@ public class UserJPADAO implements UserDAO {
 
     @Override
     public User find(Integer id) throws DAOException {
-        // EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("permissionmanagement");
-        // EntityManager entityManager = entityManagerFactory.createEntityManager();
-
         User user = null;
         try {
-            // user = entityManager.find(User.class, id);
             user = JPAUtil.getEntityManager().find(User.class, id);
         } catch (RuntimeException e) {
-            // entityManager.close();
-            // entityManagerFactory.close();
             JPAUtil.closeAll();
             String msg = "find : " + e.getMessage();
             LOG.warn(msg);
@@ -82,16 +66,13 @@ public class UserJPADAO implements UserDAO {
 
     @Override
     public User update(User user) throws DAOException {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("permissionmanagement");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
         try {
-            entityManager.getTransaction().begin();
-            entityManager.merge(user);
-            entityManager.getTransaction().commit();
+            JPAUtil.beginTransaction();
+            JPAUtil.getEntityManager().merge(user);
+            JPAUtil.commitTransaction();
         } catch (RuntimeException e) {
-            entityManager.close();
-            entityManagerFactory.close();
-            String msg = "persist : " + e.getMessage();
+            JPAUtil.closeAll();
+            String msg = "update : " + e.getMessage();
             LOG.warn(msg);
             throw new DAOException(msg, e);
         }
@@ -101,20 +82,13 @@ public class UserJPADAO implements UserDAO {
 
     @Override
     public boolean delete(Integer id) throws DAOException {
-        // EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("permissionmanagement");
-        // EntityManager entityManager = entityManagerFactory.createEntityManager();
         User user = this.find(id);
         if (null != user) {
             try {
-                // entityManager.getTransaction().begin();
-                // entityManager.remove(user);
-                // entityManager.getTransaction().commit();
                 JPAUtil.beginTransaction();
                 JPAUtil.getEntityManager().remove(user);
                 JPAUtil.commitTransaction();
             } catch (RuntimeException e) {
-                // entityManager.close();
-                // entityManagerFactory.close();
                 JPAUtil.closeAll();
                 String msg = "remove : " + e.getMessage();
                 LOG.warn(msg);
