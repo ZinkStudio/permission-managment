a5d9fb93420bf6c84790634950d036ba54600690
asus-hb <asus-hb@DESKTOP-741VTDM>
2016-07-25 15:20:42 +0200

    Modification des scopes (applicationScoped remplacé par SessionScoped), Rafraichissement des donnees du dataTable après suppression d'un user (on enleve le postConstruct de init(), on ajoute init dans getUsers()) Changement du nom du bean "UserView"

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/UserDeleteView.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/UserDeleteView.java
@@ -2,16 +2,15 @@ package fr.marseille.permissionmanagement.view;
 
 import java.io.Serializable;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import fr.marseille.permissionmanagement.exception.ServiceException;
 import fr.marseille.permissionmanagement.model.User;
 import fr.marseille.permissionmanagement.service.UserService;
-import fr.marseille.permissionmanagement.util.JPAUtil;
 
 @ManagedBean
-@ApplicationScoped
+@SessionScoped
 public class UserDeleteView implements Serializable {
     /**
      * 
@@ -20,17 +19,12 @@ public class UserDeleteView implements Serializable {
     private UserService       userService      = new UserService();
     private User              user;
 
-    public void delete() {
+    public void delete(Integer id) {
         try {
-            JPAUtil.beginTransaction();
-            JPAUtil.getEntityManager().merge(user);
-            JPAUtil.commitTransaction();
-            userService.delete(user.getId());
+            userService.delete(id);
         } catch (ServiceException e) {
-            JPAUtil.closeAll();
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Error : " + e.getMessage()));
         }
-
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User deleted"));
     }
 
@@ -41,4 +35,5 @@ public class UserDeleteView implements Serializable {
     public void setUser(User user) {
         this.user = user;
     }
+
 }
