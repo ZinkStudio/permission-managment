1c67f1bc198c0ad76915533ab62a84045ba463a7
antony <zink13013@gmail.com>
2016-07-25 14:32:19 +0200

    mise en place du faces-config

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/PermissionView.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/PermissionView.java
@@ -3,15 +3,19 @@ package fr.marseille.permissionmanagement.view;
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
 import fr.marseille.permissionmanagement.bean.PermissionController;
 import fr.marseille.permissionmanagement.model.Permission;
 
-@ManagedBean(name = "dtPermissionView")
+@ManagedBean(name = "dtPermissionEditView")
 @ViewScoped
-public class PermissionView implements Serializable {
+public class PermissionEditView implements Serializable {
 
     /**
      * 
@@ -35,5 +39,34 @@ public class PermissionView implements Serializable {
 
     public void setController(PermissionController permissionController) {
         this.controller = permissionController;
+    }
+
+    public void delete() {
+
+    }
+
+    public void onRowEdit(RowEditEvent event) {
+        Permission permission = (Permission) event.getObject();
+
+        Permission update = this.controller.update(permission);
+
+        FacesMessage msg = new FacesMessage("Permission Edited", ((Permission) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Permission) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+
+        if (newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed",
+                    "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
     }
 }
