a7d6ae4427aa6fac3e9e565e3adf0602ae9a1b13
antony <zink13013@gmail.com>
2016-07-26 21:10:38 +0200

    Suppression des beanController
    Verification de l'appel des controllers avec le raccourcis CTRL+MAJ+G
    Pour une Suppression propre de ces trois beans.

--- permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/ProfileView.java
+++ permissionmanagement/src/main/java/fr/marseille/permissionmanagement/view/ProfileView.java
@@ -4,46 +4,37 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import fr.marseille.permissionmanagement.bean.ProfileController;
 import fr.marseille.permissionmanagement.exception.ServiceException;
 import fr.marseille.permissionmanagement.model.Profile;
+import fr.marseille.permissionmanagement.service.ProfileService;
 
 @ManagedBean(name = "dtProfileView")
 @RequestScoped
 public class ProfileView implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private ProfileService    service;
     private List<Profile>     profiles;
     private Profile           profile;
-
-    @ManagedProperty("#{profileController}")
-    private ProfileController controller;
 
     // @PostConstruct
     public void init() {
         List<Profile> profiles = new ArrayList<>();
-        Profile profile = new Profile();
+        profile = new Profile();
         // profile.setName("tota");
         // profile.setDescription("dsds");
 
-        // profiles.addAll(controller.createProfiles());
+        // profiles.addAll(service.createProfiles());
 
         try {
-
-            profiles = controller.findAll();
+            profiles = service.findAll();
         } catch (ServiceException e) {
             e.printStackTrace();
         }
 
         this.profiles = profiles;
     }
-
-    // List<Profile> findAll() throws ServiceException {
-    // return profiles = controller.findAll();
-    //
-    // }
 
     public void setProfiles(List<Profile> profiles) {
         this.profiles = profiles;
@@ -52,10 +43,6 @@ public class ProfileView implements Serializable {
     public List<Profile> getProfiles() {
         init();
         return profiles;
-    }
-
-    public void setController(ProfileController controller) {
-        this.controller = controller;
     }
 
     public Profile getProfile() {
